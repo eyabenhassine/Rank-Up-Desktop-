c47479ecab98b47c45d44baa8915c1077f250566
ffare <faresbenlakdher@gmail.com>
2024-05-01 23:30:46 +0100

    added ajouter equipe

---
+++ src/main/java/com/example/rankup/tests/test.java
@@ -0,0 +1,18 @@
+package com.example.rankup.tests;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.Stage;
+
+public class test {
+
+    public static void main(String[] args) {
+
+    }
+}
+
