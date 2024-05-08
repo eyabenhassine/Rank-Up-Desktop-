25e02846ddd113da1d894bc0c9ae7283398df3db
ffare <faresbenlakdher@gmail.com>
2024-05-08 12:57:46 +0100

    pre-integration

---
+++ src/main/java/com/example/rankup/MainFX.java
@@ -0,0 +1,23 @@
+package com.example.rankup;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainFX extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/signup.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("signup");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
