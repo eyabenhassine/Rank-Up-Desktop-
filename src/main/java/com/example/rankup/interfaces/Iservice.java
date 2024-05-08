de477fd09e620c915882f48d72328647835ec931
ffare <faresbenlakdher@gmail.com>
2024-05-08 21:24:27 +0100

    added iservice

---
+++ src/main/java/com/example/rankup/interfaces/Iservice.java
@@ -0,0 +1,22 @@
+package com.example.rankup.interfaces;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface Iservice<T> {
+    void add(T t) throws SQLException;
+    void update(T t) throws SQLException;
+
+
+    void deleteC(int id) throws SQLException;
+
+
+    List<T> show() throws SQLException;
+
+
+
+    //5: one
+    T getOne(int id);
+
+
+}
