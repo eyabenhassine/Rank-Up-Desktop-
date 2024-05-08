af7ce665d5bc6c70eb1ce7ecaed82040b21f1e3b
ffare <faresbenlakdher@gmail.com>
2024-05-08 12:59:50 +0100

    pre-integration

--- src/main/java/com/example/rankup/utils/DataSource.java
+++ src/main/java/com/example/rankup/utils/DataSource.java
@@ -1,11 +1,7 @@
 package com.example.rankup.utils;
-
-
-
-
-        import java.sql.Connection;
-        import java.sql.DriverManager;
-        import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DataSource {
     final String URL = "jdbc:mysql://localhost:3306/rankup";
